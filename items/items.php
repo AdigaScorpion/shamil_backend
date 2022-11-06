d4bde4d3f6939fac58f060511be4eb42d5226bdb
AdigaScorpion <AdigaScorpion@live.com>
2022-11-06 22:44:54 +0300

    Shamil_Backend

--- items/items.php
+++ items/items.php
@@ -2,7 +2,26 @@
 include "../connect.php";
 
 $categoryid = filterRequest("id");
+$userid = filterRequest("userid");
 
-getAllData( "itemsview" , "categories_id = $categoryid") ;
+// getAllData( "itemsview" , "categories_id = $categoryid") ;
+
+$stmt = $con->prepare("SELECT itemsview.* , '1' AS favorite FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = $userid 
+WHERE categories_id = $categoryid
+UNION All
+SELECT *, '0' as favorite FROM itemsview 
+WHERE categories_id = $categoryid AND items_id NOT IN  (SELECT itemsview.items_id FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = items_id AND favorite.favorite_usersid = $userid )");
+
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $count  = $stmt->rowCount();
+    if ($count > 0){
+        echo json_encode(array("status" => "success PHP", "data" => $data));
+    } else {
+        echo json_encode(array("status" => "failure PHP"));
+    }
+    
 
 ?> 
--- view.sql
+++ view.sql
@@ -1,5 +1,20 @@
 CREATE OR REPLACE VIEW itemsview AS
 SELECT items.* , categories.* FROM items 
-INNER JOIN categories.* ON categories.categories_id = items.items_cat 
+INNER JOIN categories ON categories.categories_id = items.items_cat 
 
+
+SELECT itemsview.* FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = users_id
+
+
+SELECT itemsview.* , 1 AS favorite FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = users_id
+
+
+SELECT itemsview.* , 1 AS favorite FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = users_id
+UNION All
+SELECT *, 0 as favorite FROM itemsview 
+WHERE items_id NOT IN  (SELECT itemsview.items_id FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = items_id AND favorite.favorite_usersid = users_id)
 
