50e2bb2b6cf0a150fa148c0d21520d28c03796b6
AdigaScorpion <AdigaScorpion@live.com>
2022-12-08 09:26:18 +0300

    stage 2

--- view.sql
+++ view.sql
@@ -11,10 +11,10 @@ SELECT itemsview.* , 1 AS favorite FROM itemsview
 INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = users_id
 
 
-SELECT itemsview.* , 1 AS favorite FROM itemsview
-INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = users_id
+SELECT itemsview.* , '1' AS favorite FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = itemsview.items_id AND favorite.favorite_usersid = $userid 
+WHERE categories_id = $categoryid
 UNION All
-SELECT *, 0 as favorite FROM itemsview 
-WHERE items_id NOT IN  (SELECT itemsview.items_id FROM itemsview
-INNER JOIN favorite ON favorite.favorite_itemsid = items_id AND favorite.favorite_usersid = users_id)
-
+SELECT *, '0' as favorite FROM itemsview 
+WHERE categories_id = $categoryid AND items_id NOT IN  (SELECT itemsview.items_id FROM itemsview
+INNER JOIN favorite ON favorite.favorite_itemsid = items_id AND favorite.favorite_usersid = $userid )
